ce2de11bbc8ffe40e6e3dff50892e615feef8e9a
Reid Mason <maddogshain132@gmail.com>
2023-08-22 05:30:08 +0100

    Expanded karma count to scan more messages and send result

--- main.go
+++ main.go
@@ -18,11 +18,12 @@ const RemoveCommands = true
 const UpvoteEmoji = "😂"
 
 type UserProfile struct {
-	ID    string
-	Karma int
+	ID       string
+	Username string
+	Karma    int
 }
 
-func addKarma(userId string, users *[]UserProfile, amount int) {
+func addKarma(userId string, username string, users *[]UserProfile, amount int) {
 	for _, user := range *users {
 		if user.ID == userId {
 			user.Karma += amount
@@ -30,7 +31,7 @@ func addKarma(userId string, users *[]UserProfile, amount int) {
 		}
 	}
 
-	*users = append(*users, UserProfile{ID: userId, Karma: amount})
+	*users = append(*users, UserProfile{ID: userId, Username: username, Karma: amount})
 }
 
 type Command struct {
@@ -86,26 +87,56 @@ var commands = []Command{
 		Description: "Calculate karma score of users",
 		Type:        discordgo.ApplicationCommandType(1),
 		Handler: func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
+			})
+
 			channelId := i.ChannelID
-			messages, err := s.ChannelMessages(channelId, 100, "", "", "")
-			if err != nil {
-				log.Println(err)
+
+			messages := make([]*discordgo.Message, 0)
+			messageBatches := 10
+			messagesPerBatch := 100
+			lastMessageId := ""
+			log.Print("Getting messages")
+			for i := 0; i < messageBatches; i++ {
+				newMessages, err := s.ChannelMessages(channelId, messagesPerBatch, lastMessageId, "", "")
+				if err == nil {
+					messages = append(messages, newMessages...)
+				}
+				lastMessageId = newMessages[len(newMessages)-1].ID
 			}
 
 			users := make([]UserProfile, 0)
 
+			log.Print("Calculating karma")
 			for _, message := range messages {
 				reactions := message.Reactions
 				for _, reaction := range reactions {
 					if reaction.Emoji.Name == UpvoteEmoji {
-						addKarma(message.Author.ID, &users, 1)
+						addKarma(message.Author.ID, message.Author.Username, &users, 1)
 					}
 				}
 			}
 
+			log.Print("Building fields")
+			fields := make([]*discordgo.MessageEmbedField, 0)
 			for _, user := range users {
-				log.Println(user.ID, user.Karma)
+				fields = append(fields, &discordgo.MessageEmbedField{
+					Name:  user.Username,
+					Value: fmt.Sprintf("Karma: %d", user.Karma),
+				})
 			}
+
+			log.Print("Sending message")
+			s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
+				Embeds: &[]*discordgo.MessageEmbed{
+					{
+						Title:       "Karma count",
+						Description: fmt.Sprintf("Last %d messages", len(messages)),
+						Fields:      fields,
+					},
+				},
+			})
 		},
 	},
 	{
